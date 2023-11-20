44bc2aaab8367614c5b87f11a82422aecb119ae5
FengJima <130929301+FengJima@users.noreply.github.com>
2023-11-20 08:59:07 +0800

    Add files via upload

---
+++ src/test/java/com/kary/hahaha3/controller/loginAndRegister/LoginControllerTest.java
@@ -0,0 +1,89 @@
+package com.kary.hahaha3.controller.loginAndRegister;
+
+import com.kary.hahaha3.controller.loginAndRegister.LoginController;
+import com.kary.hahaha3.mapper.UserMapper;
+import com.kary.hahaha3.pojo.User;
+import com.kary.hahaha3.utils.AESUtil;
+import jakarta.servlet.http.HttpSession;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.verification.VerificationMode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.ui.Model;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+public class LoginControllerTest {
+    @Autowired
+    @Qualifier("AESEncoder")
+    AESUtil aesEncoder;
+    @Autowired
+    UserMapper userMapper;
+    @Autowired
+    private LoginController loginController;
+    private User myAccount;
+    //登录成功
+    @Test
+    public void testLoginSuccess() {
+        // Mock dependencies
+        User user = new User();
+        user.setUsername("testLoginSuccessName");
+        user.setPwd("testLoginSuccessPwd");
+        Model model = mock(Model.class);
+        HttpSession session = mock(HttpSession.class);
+        String result = loginController
+                .login(user.getUsername(), user.getPwd(), model, session);
+        // Assertions
+        assertEquals("views/loginSuccess", result);
+        myAccount = new User();
+        User userInDatabase = userMapper.selectUserByName(user.getUsername());
+        myAccount.setUsername(user.getUsername());
+        myAccount.setPwd(user.getPwd());
+        myAccount.setScoreTotal(userInDatabase.getScoreTotal());
+        myAccount.setEmail(userInDatabase.getEmail());
+        myAccount.setRegdate(userInDatabase.getRegdate());
+        myAccount.setGameCount(userInDatabase.getGameCount());
+        myAccount.setGameId(userInDatabase.getGameId());
+        verify(model).addAttribute("myAccount", myAccount);
+        verify(session).setAttribute("myAccount",myAccount);
+    }
+    //未注册登录
+    @Test
+    public void testLoginWithoutRegister(){
+        // Mock dependencies
+        User user = new User();
+        user.setUsername("NotRegisteredName");
+        user.setPwd("NotRegisterPwd");
+        Model model = mock(Model.class);
+        HttpSession session = mock(HttpSession.class);
+        String result = loginController
+                .login(user.getUsername(), user.getPwd(), model, session);
+
+        // Assertions
+        assertEquals("views/login", result);
+        verify(model).addAttribute("showPopup","该用户未注册");
+    }
+    //密码错误登录
+    @Test
+    public void testLoginPwdError(){
+        // Mock dependencies
+        User user = new User();
+        user.setUsername("testLoginSuccessName");
+        user.setPwd("testLoginErrorPwd");
+        Model model = mock(Model.class);
+        HttpSession session = mock(HttpSession.class);
+        String result = loginController
+                .login(user.getUsername(), user.getPwd(), model, session);
+
+        // Assertions
+        assertEquals("views/login", result);
+        verify(model).addAttribute("showPopup","密码错误");
+    }
+}
---
+++ src/test/java/com/kary/hahaha3/controller/loginAndRegister/RegisterControllerTest.java
@@ -0,0 +1,89 @@
+package com.kary.hahaha3.controller.loginAndRegister;
+
+import com.kary.hahaha3.HaHaHa3Application;
+import com.kary.hahaha3.mapper.UserMapper;
+import com.kary.hahaha3.pojo.User;
+import com.kary.hahaha3.utils.AESUtil;
+import com.kary.hahaha3.utils.MailUtil;
+import jakarta.servlet.http.HttpSession;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@SpringBootTest
+public class RegisterControllerTest {
+    @Autowired
+    @Qualifier("AESEncoder")
+    AESUtil aesEncoder;
+    @Autowired
+    UserMapper userMapper;
+    @Autowired
+    RegisterController registerController;
+    @Mock
+    HttpSession httpSession;
+    private MockMvc mockMvc;
+    @Mock
+    Model model;
+
+    private User user;
+    @Test
+    void testRegisterWithoutUsername(){
+        String result = registerController.userRegister(null,"pwd","pwd","email",model, httpSession);
+        assertEquals("views/register", result);
+        verify(model).addAttribute("showPopup", "用户名为空");
+    }
+    @Test
+    void testRegisterWithoutPassword(){
+        String result = registerController.userRegister("username",null,"pwd","email",model,httpSession);
+        assertEquals("views/register",result);
+        verify(model).addAttribute("showPopup", "密码为空");
+    }
+    @Test
+    void testRegisterWithoutRetypePassword(){
+        String result = registerController.userRegister("username","pwd",null,"email",model,httpSession);
+        assertEquals("views/register",result);
+        verify(model).addAttribute("showPopup", "请重输密码");
+    }
+    @Test
+    void testRegisterPasswordMismatched(){
+        String result = registerController.userRegister("username","pwd","mismatched_pwd","email",model,httpSession);
+        assertEquals("views/register",result);
+        verify(model).addAttribute("showPopup", "请输入一致的密码");
+    }
+    @Test
+    void testRegisterUserRegistered(){
+        String result = registerController.userRegister("testRegisteredUser","pwd","pwd","email",model,httpSession);
+        assertEquals("views/register",result);
+        verify(model).addAttribute("showPopup","该用户已注册");
+    }
+    @Test
+    void testRegisterIllegalQQMail(){
+        String result = registerController.userRegister("testRegisteredUser","pwd","pwd","IllegalEmail",model,httpSession);
+        assertEquals("views/register",result);
+        verify(model).addAttribute("showPopup","请输入合法的邮箱");
+    }
+    @Test
+    void testRegisterSuccess(){
+        //陈金萍的邮箱
+        String email = "2452826804@qq.com";
+        String result = registerController.userRegister("testRegisterUsername","testRegisterPwd","testRegisterPwd",email,model,httpSession);
+        assertEquals("views/emailVerification",result);
+        verify(httpSession).setAttribute("username","testRegisterUsername");
+        verify(httpSession).setAttribute("password","testRegisterPwd");
+        verify(httpSession).setAttribute("email",email);
+    }
+}
