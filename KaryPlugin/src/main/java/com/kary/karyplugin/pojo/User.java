57a58e73d1a16a59e4b9aaa37bc8c97ffcf6ff32
karywood <834479572@qq.com>
2023-11-25 15:11:57 +0800

    无数据库

--- KaryPlugin/src/main/java/com/kary/karyplugin/pojo/User.java
+++ KaryPlugin/src/main/java/com/kary/karyplugin/pojo/User.java
@@ -15,21 +15,6 @@ public class User {
     private String email;
     private Date regdate;
     private int gamesCount;
-    private String gamesId;
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "username='" + username + '\'' +
-                ", pwd='" + pwd + '\'' +
-                ", scoreTotal1v1=" + scoreTotal1v1 +
-                ", scoreTotalBrawl=" + scoreTotalBrawl +
-                ", email='" + email + '\'' +
-                ", regdate=" + regdate +
-                ", gamesCount=" + gamesCount +
-                ", gamesId='" + gamesId + '\'' +
-                '}';
-    }
 
     public String getUsername() {
         return username;
@@ -87,15 +72,7 @@ public class User {
         this.gamesCount = gamesCount;
     }
 
-    public String getGamesId() {
-        return gamesId;
-    }
-
-    public void setGamesId(String gamesId) {
-        this.gamesId = gamesId;
-    }
-
-    public User(String username, String pwd, int scoreTotal1v1, int scoreTotalBrawl, String email, Date regdate, int gamesCount, String gamesId) {
+    public User(String username, String pwd, int scoreTotal1v1, int scoreTotalBrawl, String email, Date regdate, int gamesCount) {
         this.username = username;
         this.pwd = pwd;
         this.scoreTotal1v1 = scoreTotal1v1;
@@ -103,6 +80,8 @@ public class User {
         this.email = email;
         this.regdate = regdate;
         this.gamesCount = gamesCount;
-        this.gamesId = gamesId;
+    }
+
+    public User() {
     }
 }
