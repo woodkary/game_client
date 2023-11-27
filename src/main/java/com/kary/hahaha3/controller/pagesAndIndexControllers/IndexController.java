695942ef1c7cfe8db5ec24ac87dd687f40efaf49
karywood <834479572@qq.com>
2023-11-28 00:06:44 +0800

    重构代码，加入json返回和错误处理

--- src/main/java/com/kary/hahaha3/controller/pagesAndIndexControllers/IndexController.java
+++ src/main/java/com/kary/hahaha3/controller/pagesAndIndexControllers/IndexController.java
@@ -1,11 +1,10 @@
 package com.kary.hahaha3.controller.pagesAndIndexControllers;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.kary.hahaha3.controller.BaseController;
-import com.kary.hahaha3.exceptions.JsonException;
 import com.kary.hahaha3.exceptions.expired.SessionExpireException;
 import com.kary.hahaha3.pojo.JsonResult;
 import com.kary.hahaha3.pojo.User;
+import com.kary.hahaha3.pojo.vo.RecordVO;
 import com.kary.hahaha3.service.RecordVOService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -15,7 +14,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
 
 /**
  * @author:123
@@ -34,12 +33,7 @@ public class IndexController extends BaseController {
         if(myAccount==null){
             throw new SessionExpireException("你尚未登录");
         }
-        String recordVOJson;
-        try {
-            recordVOJson = new ObjectMapper().writeValueAsString(recordVOService.getGamesByIds(myAccount.getUsername(), 1,1));
-        } catch (JsonProcessingException e) {
-            throw new JsonException("返回比赛记录错误",e);
-        }
-        return JsonResult.ok(recordVOJson,"这是战绩");
+        List<RecordVO> recordVO=recordVOService.getGamesByIds(myAccount.getUsername(), 1,1);
+        return JsonResult.ok(recordVO,"这是战绩");
     }
 }
--- src/main/java/com/kary/hahaha3/pojo/JsonResult.java
+++ src/main/java/com/kary/hahaha3/pojo/JsonResult.java
@@ -18,21 +18,21 @@ import java.io.Serializable;
 @EqualsAndHashCode
 public class JsonResult implements Serializable {
     private int code;
-    private Serializable data;
+    private Object data;
     private String message;
-    public static JsonResult ok(Serializable data,String message){
+    public static JsonResult ok(Object data,String message){
         return new JsonResult(200,data,message);
     }
-    public static JsonResult ok(Serializable data){
+    public static JsonResult ok(Object data){
         return ok(data,"");
     }
     public static JsonResult ok(String message){
         return ok("", message);
     }
-    public static JsonResult error(Serializable data,String message){
+    public static JsonResult error(Object data,String message){
         return new JsonResult(400,data,message);
     }
-    public static JsonResult error(Serializable data){
+    public static JsonResult error(Object data){
         return error(data,"错误，请重试");
     }
     public static JsonResult error(String message){
