70cc6ff417b6a54166e81deea8c9b9a23add7034
karywood <834479572@qq.com>
2023-11-28 17:45:27 +0800

    重构代码，加入json返回和错误处理

--- src/main/java/com/kary/hahaha3/mapper/ArticleMapper.java
+++ src/main/java/com/kary/hahaha3/mapper/ArticleMapper.java
@@ -6,12 +6,10 @@ import org.apache.ibatis.annotations.Param;
  * @author:123
  */
 public interface ArticleMapper {
-    Integer publishArticle(@Param("articleId")Integer articleId,
-                           @Param("username")String username,
+    Integer publishArticle(@Param("username")String username,
                            @Param("content")String content,
                            @Param("articleTopic")String articleTopic,
                            @Param("themeName")String themeName);
-    Integer getMaxArticleId();
     Integer follow(@Param("articleId")Integer articleId);
     Integer like(@Param("articleId")Integer articleId);
     Integer dislike(@Param("articleId")Integer articleId);
--- src/main/java/com/kary/hahaha3/mapper/CommentMapper.java
+++ src/main/java/com/kary/hahaha3/mapper/CommentMapper.java
@@ -1,7 +1,14 @@
 package com.kary.hahaha3.mapper;
 
+import org.apache.ibatis.annotations.Param;
+
 /**
  * @author:123
  */
 public interface CommentMapper {
+    //TODO 利用Service层判断commentFlag的值
+    Integer publishComment(@Param("username")String username,
+                           @Param("articleId")Integer articleId,
+                           @Param("content")String content,
+                            @Param("commentFlag")Integer commentFlag);
 }
