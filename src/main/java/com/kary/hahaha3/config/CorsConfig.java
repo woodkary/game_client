a829d5a12ff6c56a649e680d944fb0fbf64edfc3
karywood <834479572@qq.com>
2023-12-02 21:45:20 +0800

    设置跨域资源共享

---
+++ src/main/java/com/kary/hahaha3/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.kary.hahaha3.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author:123
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        /*
+         * 一小时内不需要再预检
+         */
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8081","http://127.0.0.1:8081")
+                .allowCredentials(true)
+                .allowedHeaders(CorsConfiguration.ALL)
+                .allowedMethods(CorsConfiguration.ALL)
+                .maxAge(3600);
+    }
+}
