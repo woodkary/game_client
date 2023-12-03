bd027de3a76e63a4f81d94c11e55c1fa52eb49d1
caster0011 <147359979+caster0011@users.noreply.github.com>
2023-12-03 15:17:28 +0800

    Update record.css

--- src/test/java/com/kary/hahaha3/controller/loginAndRegister/LoginControllerTest.java
+++ src/test/java/com/kary/hahaha3/controller/loginAndRegister/LoginControllerTest.java
@@ -1,50 +1,56 @@
 package com.kary.hahaha3.controller.loginAndRegister;
 
 import com.kary.hahaha3.exceptions.errorInput.ErrorInputException;
+import com.kary.hahaha3.exceptions.errorInput.PasswordErrorException;
+import com.kary.hahaha3.exceptions.errorInput.UsernameErrorException;
 import com.kary.hahaha3.mapper.UserMapper;
 import com.kary.hahaha3.pojo.JsonResult;
 import com.kary.hahaha3.pojo.User;
+import com.kary.hahaha3.service.UserService;
 import com.kary.hahaha3.utils.AESUtil;
 import jakarta.servlet.http.HttpSession;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
 public class LoginControllerTest {
     @Autowired
+    UserMapper userMapper;
+    @Autowired
     @Qualifier("AESEncoder")
     AESUtil aesEncoder;
     @Autowired
-    UserMapper userMapper;
+    private UserService userService;
     @Autowired
     private LoginController loginController;
-    private User myAccount;
-    //登录成功
+
+
     @Test
     public void testLoginSuccess() throws ErrorInputException {
-        // Mock dependencies
         User user = new User();
-        user.setUsername("kary");
-        user.setPwd("wood20040629");
+        user.setUsername("testLoginSuccessName");
+        user.setPwd("testLoginSuccessPwd");
         HttpSession session = mock(HttpSession.class);
         JsonResult result = loginController
                 .login(user.getUsername(), user.getPwd(),session);
 
-        myAccount = new User();
-        User userInDatabase = userMapper.selectUserByName(user.getUsername());
+        User myAccount = new User();
+        User userInDatabase = userService.selectUserByName(user.getUsername());
         myAccount.setUsername(user.getUsername());
         myAccount.setPwd(user.getPwd());
-        myAccount.setScoreTotal1v1(userInDatabase.getScoreTotal1v1());
         myAccount.setEmail(userInDatabase.getEmail());
         myAccount.setRegdate(userInDatabase.getRegdate());
-        myAccount.setGamesCount(userInDatabase.getGamesCount());
         JsonResult expectedResult=JsonResult.ok(myAccount,"登录成功");
         // Assertions
         assertEquals(expectedResult, result);
@@ -53,18 +59,11 @@ public class LoginControllerTest {
     //未注册登录
     @Test
     public void testLoginWithoutRegister() throws ErrorInputException {
-        // Mock dependencies
         User user = new User();
         user.setUsername("NotRegisteredName");
         user.setPwd("NotRegisterPwd");
         HttpSession session = mock(HttpSession.class);
-        JsonResult result = loginController
-                .login(user.getUsername(), user.getPwd(), session);
-        JsonResult expectedResult=JsonResult.error("用户不存在");
-
-        // Assertions
-        assertEquals(expectedResult, result);
-        /*verify(model).addAttribute("showPopup","该用户未注册");*/
+        assertThrows(new UsernameErrorException("用户不存在").getClass(),()->loginController.login(user.getUsername(), user.getPwd(), session));
     }
     //密码错误登录
     @Test
@@ -74,12 +73,6 @@ public class LoginControllerTest {
         user.setUsername("testLoginSuccessName");
         user.setPwd("testLoginErrorPwd");
         HttpSession session = mock(HttpSession.class);
-        JsonResult result = loginController
-                .login(user.getUsername(), user.getPwd(),session);
-        JsonResult expectedResult=JsonResult.error("密码错误");
-
-        // Assertions
-        assertEquals("views/login", result);
-        /*verify(model).addAttribute("showPopup","密码错误");*/
+        assertThrows(new PasswordErrorException("密码错误").getClass(),()->loginController.login(user.getUsername(), user.getPwd(), session));
     }
 }
