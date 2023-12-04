3afcbcc87063f341f5996aa1fae952403c4ad234
karywood <834479572@qq.com>
2023-12-04 14:30:56 +0800

    设置比赛记录

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,28 +1,8 @@
 
 
 function search() {
-  const username = document.getElementById("username").value;
-  console.log(username);
-  var search = window.location.search;
-  var params = new URLSearchParams(search);
-  var myUsername = params.get("username");
-  document.getElementById("myUsername").setValue(myUsername);
-
-  if (username == 'danqingyifan') {
-    window.location.href = "pages/personal.html";
-  } else {
-    var url = 'http://localhost:8080/ranks/getRanks/1?username=' + encodeURIComponent(data.username);
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        window.location.href = "pages/personal/search?username=" + encodeURIComponent(data.username) + "&page=1";
-        }
-      )
-      .catch(error => {
-        console.error(error);
-      });
-  }
+  let username = document.getElementById("username").value;
+  window.location.href = "pages/personal.html?username="+username;
 }
 
 document.getElementById("username").addEventListener("keydown",
