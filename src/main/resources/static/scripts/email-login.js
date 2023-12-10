b45c9d5a7314c931553ab30a57cde5940bf4019c
karywood <834479572@qq.com>
2023-12-10 21:49:46 +0800

    修改邮箱登录js

--- src/main/resources/static/scripts/email-login.js
+++ src/main/resources/static/scripts/email-login.js
@@ -2,10 +2,15 @@
 document.getElementById("login-form").addEventListener("submit", validateLogin);
 
 function validateLogin() {
-    var xmlHttpRequest = new XMLHttpRequest();
+    let veriCode=document.getElementById("captcha").textContent;
+    veriCode=JSON.stringify(veriCode);
+
+    let xmlHttpRequest = new XMLHttpRequest();
+    xmlHttpRequest.withCredentials=true;
     xmlHttpRequest.onreadystatechange = function () { // 设置响应http请求状态变化的事件
-        var jsonResult = JSON.parse(xmlHttpRequest.responseText);
-        var message = document.getElementById("verification-message");
+        let jsonResult = JSON.parse(xmlHttpRequest.responseText);
+        let message = document.getElementById("verification-message");
+
         if (xmlHttpRequest.status == 200) {
             message.textContent = jsonResult.message;
             message.style.color = "green";
@@ -16,13 +21,12 @@ function validateLogin() {
         }
     }
 
-    var email = document.getElementById("email").value;
-
+    /*var email = document.getElementById("email").value;*/
     /*let requestData = { username: username, password: password };
     requestData=JSON.stringify(requestData);*/
 
-    xmlHttpRequest.open("GET", "http://localhost:8080/login?username=" + username + "&password=" + password, true); // 创建http请求，并指定请求得方法（get）、url（https://www.runoob.com/try/ajax/ajax_info.txt）以及验证信息
-    xmlHttpRequest.send(null); // 发送请求
+    xmlHttpRequest.open("POST", "http://localhost:8080/typeVeriCode/3", true); // 创建http请求，并指定请求得方法（get）、url（https://www.runoob.com/try/ajax/ajax_info.txt）以及验证信息
+    xmlHttpRequest.send(veriCode); // 发送请求
 
     //Post email to server
 }
