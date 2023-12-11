7341e6bef5323afed216e4642e4a781b8668e5ec
karywood <834479572@qq.com>
2023-12-11 15:42:15 +0800

    修改邮箱登录js

--- src/main/java/com/kary/hahaha3/utils/LevelUtil.java
+++ src/main/java/com/kary/hahaha3/utils/LevelUtil.java
@@ -4,15 +4,10 @@ package com.kary.hahaha3.utils;
  * @author:123
  */
 public class LevelUtil {
-    public static final String[] levels=new String[]{"","青铜","白银","黄金","白金","钻石","王者"};
+    public static final String[] levels=new String[]{"青铜","白银","黄金","白金","钻石","大师","王者"};
 
     public static String getLevel(int score){
-        if(score>=1800){
-            return levels[6];
-        }else if(score>=1500){
-            return levels[5];
-        }
         int i=score/300;
-        return levels[i+1];
+        return levels[i];
     }
 }
