54e8cca67dc71f9e3d9c18cb350a90b25516c3d1
Danqingyifan <danqingyifan@gmail.com>
2023-12-12 21:36:26 +0800

    adjust personal layout

--- src/main/resources/static/scripts/personal.js
+++ src/main/resources/static/scripts/personal.js
@@ -1,43 +1,29 @@
-let username=null;
-function preLoad(){
-    let query=window.location.search;
-    let params=new URLSearchParams(query);
-    username=params.get("username");
-}
+let username = null;
 preLoad();
-function setInputDataPersonalSin(data) {
-    document.getElementById("gameNumsSin").textContent = data.gameNums;
-    document.getElementById("winSin").textContent = data.win;
-    document.getElementById("loseSin").textContent = data.lose;
-    document.getElementById("winRateSin").textContent = data.winRate;
-    document.getElementById("levelSin").textContent = data.level;
-}
-function setInputDataPersonalBrawl(data) {
-    document.getElementById("gameNumsBrawl").textContent = data.gameNums;
-    document.getElementById("winBrawl").textContent = data.win;
-    document.getElementById("loseBrawl").textContent = data.lose;
-    document.getElementById("winRateBrawl").textContent = data.winRate;
-    document.getElementById("levelBrawl").textContent = data.level;
-}
+handleDataPersonal(1);
+handleDataPersonal(2);
+handleDataAll();
+handleDataMonth();
 
-function handleDataPersonal(type)
-{
+
+
+function handleDataPersonal(type) {
     let xhr = new XMLHttpRequest();
-    xhr.withCredentials=true;
+    xhr.withCredentials = true;
 
-    let url='http://localhost:8080/ranks/othersReport/'+type+'?username='+encodeURIComponent(username);
+    let url = 'http://localhost:8080/ranks/othersReport/' + type + '?username=' + encodeURIComponent(username);
     // 配置请求
     xhr.open('GET', url); // 根据您的实际需求指定适当的URL
     xhr.setRequestHeader('Content-Type', 'application/json');
 
-    xhr.onload = function() {
+    xhr.onload = function () {
         let response = xhr.responseText;
-        let jsonResult=JSON.parse(response);
+        let jsonResult = JSON.parse(response);
         if (xhr.status === 200) {
-            let data=jsonResult.data;
-            if(type===1)
+            let data = jsonResult.data;
+            if (type === 1)
                 setInputDataPersonalSin(data);
-            if(type===2)
+            if (type === 2)
                 setInputDataPersonalBrawl(data);
         } else {
             console.log(jsonResult.message);
@@ -45,33 +31,23 @@ function handleDataPersonal(type)
     };
     xhr.send();
 }
-handleDataPersonal(1);
-handleDataPersonal(2);
-
-function setInputDataAll(data) {
-    document.getElementById("kdaAll").textContent = data.kda;
-    document.getElementById("winRateAll").textContent = data.winRate;
-    document.getElementById("totalKillsAll").textContent = data.totalKills;
-    document.getElementById("gameNumsAll").textContent = data.gameNums;
-}
 
 
-function handleDataAll()
-{
+function handleDataAll() {
     let xhr = new XMLHttpRequest();
-    xhr.withCredentials=true;
+    xhr.withCredentials = true;
 
-    let url='http://localhost:8080/ranks/othersAllRecords'+'?username='+encodeURIComponent(username);
+    let url = 'http://localhost:8080/ranks/othersAllRecords' + '?username=' + encodeURIComponent(username);
 
     // 配置请求
     xhr.open('GET', url); // 根据您的实际需求指定适当的URL
     xhr.setRequestHeader('Content-Type', 'application/json');
 
-    xhr.onload = function() {
+    xhr.onload = function () {
         let response = xhr.responseText;
-        let jsonResult=JSON.parse(response);
+        let jsonResult = JSON.parse(response);
         if (xhr.status === 200) {
-            let data=jsonResult.data;
+            let data = jsonResult.data;
             setInputDataAll(data);
         } else {
             console.log(err);
@@ -79,30 +55,24 @@ function handleDataAll()
     };
     xhr.send();
 }
-handleDataAll();
-function setInputDataMonth(data) {
-    document.getElementById("kdaMonth").textContent = data.kda;
-    document.getElementById("winRateMonth").textContent = data.winRate;
-    document.getElementById("totalKillsMonth").textContent = data.totalKills;
-    document.getElementById("gameNumsMonth").textContent = data.gameNums;
-}
 
 
-function handleDataMonth()
-{
+
+
+function handleDataMonth() {
     let xhr = new XMLHttpRequest();
-    xhr.withCredentials=true;
-    let url='http://localhost:8080/ranks/othersMonthRecords'+'?username='+encodeURIComponent(username);
+    xhr.withCredentials = true;
+    let url = 'http://localhost:8080/ranks/othersMonthRecords' + '?username=' + encodeURIComponent(username);
 
     // 配置请求
     xhr.open('GET', url); // 根据您的实际需求指定适当的URL
     xhr.setRequestHeader('Content-Type', 'application/json');
 
-    xhr.onload = function() {
+    xhr.onload = function () {
         let response = xhr.responseText;
-        let jsonResult=JSON.parse(response);
+        let jsonResult = JSON.parse(response);
         if (xhr.status === 200) {
-            let data=jsonResult.data;
+            let data = jsonResult.data;
             setInputDataMonth(data);
         } else {
             console.log(err);
@@ -110,21 +80,20 @@ function handleDataMonth()
     };
     xhr.send();
 }
-handleDataMonth();
 
 function getRanks(username, page) {
     // 构建URL
-    var url = "/ranks/getRanks/" + page + "?username=" + encodeURIComponent(username)+"&page=" + encodeURIComponent(page) ;
+    var url = "/ranks/getRanks" + "?username=" + encodeURIComponent(username);
     // 发送GET请求
     fetch(url)
-        .then(function(response) {
+        .then(function (response) {
             return response.json();
         })
-        .then(function(data) {
+        .then(function (data) {
             // 调用populateTable函数以填充表格
             setInputDataRecent(data);
         })
-        .catch(function(error) {
+        .catch(function (error) {
             console.log(error);
         });
 }
@@ -158,21 +127,21 @@ function setInputDataRecent(data) {
         }
         row.appendChild(resultCell);
         resultCell.setAttribute("class", "td3"); // 添加 class 属性
-        resultCell.setAttribute("id", "result"+i); // 添加 id 属性
+        resultCell.setAttribute("id", "result" + i); // 添加 id 属性
         resultCell.setAttribute("width", "20px"); // 添加 width 属性
         row.appendChild(resultCell);
 
         var typeCell = document.createElement("td");
         typeCell.textContent = record.type;
         typeCell.setAttribute("class", "td3"); // 添加 class 属性
-        typeCell.setAttribute("id", "type"+i); // 添加 id 属性
+        typeCell.setAttribute("id", "type" + i); // 添加 id 属性
         typeCell.setAttribute("width", "20px"); // 添加 width 属性
         row.appendChild(typeCell);
 
         var timeCell = document.createElement("td");
         timeCell.textContent = record.gameTime;
         timeCell.setAttribute("class", "td3"); // 添加 class 属性
-        timeCell.setAttribute("id", "gameTime"+i); // 添加 id 属性
+        timeCell.setAttribute("id", "gameTime" + i); // 添加 id 属性
         timeCell.setAttribute("width", "100px"); // 添加 width 属性
         row.appendChild(timeCell);
 
@@ -181,14 +150,38 @@ function setInputDataRecent(data) {
     }
 }
 
-// 调用函数，传递username和page
-// 获取当前页面的查询字符串部分
-var search = window.location.search;
-
-// 使用URLSearchParams来解析查询字符串
-var params = new URLSearchParams(search);
-
-// 从查询字符串中获取用户名
-var username = params.get('username');
-var page = 1;
-getRanks(username, page);
+function setInputDataPersonalSin(data) {
+    document.getElementById("gameNumsSin").textContent = data.gameNums;
+    document.getElementById("winSin").textContent = data.win;
+    document.getElementById("loseSin").textContent = data.lose;
+    document.getElementById("winRateSin").textContent = data.winRate;
+    document.getElementById("levelSin").textContent = data.level;
+}
+function setInputDataPersonalBrawl(data) {
+    document.getElementById("gameNumsBrawl").textContent = data.gameNums;
+    document.getElementById("winBrawl").textContent = data.win;
+    document.getElementById("loseBrawl").textContent = data.lose;
+    document.getElementById("winRateBrawl").textContent = data.winRate;
+    document.getElementById("levelBrawl").textContent = data.level;
+}
+function setInputDataAll(data) {
+    document.getElementById("kdaAll").textContent = data.kda;
+    document.getElementById("winRateAll").textContent = data.winRate;
+    document.getElementById("totalKillsAll").textContent = data.totalKills;
+    document.getElementById("gameNumsAll").textContent = data.gameNums;
+}
+function setInputDataMonth(data) {
+    document.getElementById("kdaMonth").textContent = data.kda;
+    document.getElementById("winRateMonth").textContent = data.winRate;
+    document.getElementById("totalKillsMonth").textContent = data.totalKills;
+    document.getElementById("gameNumsMonth").textContent = data.gameNums;
+}
+function preLoad() {
+    let query = window.location.search;
+    let params = new URLSearchParams(query);
+    username = params.get("username");
+    console.log(username);
+}
+function redircetToRecord() {
+    window.location.href = "../pages/record.html?username=" + encodeURIComponent(username);
+}
