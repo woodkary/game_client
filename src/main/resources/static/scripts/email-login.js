c18813de8c3c8f2c6c7d393bc7a42e3828a15d0f
Danqingyifan <danqingyifan@gmail.com>
2023-12-14 20:07:26 +0800

    email-login update

--- src/main/resources/static/scripts/email-login.js
+++ src/main/resources/static/scripts/email-login.js
@@ -1,17 +1,17 @@
 
 function validateLogin(event) {
     event.preventDefault();
-    let veriCode=document.getElementById("captcha").value;
-    veriCode=JSON.parse(veriCode);
+    let veriCode = document.getElementById("captcha").value;
+    let veriCodeJson = JSON.stringify(veriCode);
 
     let xmlHttpRequest = new XMLHttpRequest();
-    xmlHttpRequest.withCredentials=true;
+    xmlHttpRequest.withCredentials = true;
     xmlHttpRequest.onreadystatechange = function () { // 设置响应http请求状态变化的事件
         let jsonResult = JSON.parse(xmlHttpRequest.responseText);
         let message = document.getElementById("verification-message");
         if (xmlHttpRequest.status === 200) {
             message.textContent = jsonResult.message;
-            let username=jsonResult.data.username;
+            let username = jsonResult.data.username;
             message.style.color = "green";
             redirectToIndexPage(username);
         } else {
@@ -19,31 +19,24 @@ function validateLogin(event) {
             message.style.color = "red";
         }
     }
-
-    /*var email = document.getElementById("email").value;*/
-    /*let requestData = { username: username, password: password };
-    requestData=JSON.stringify(requestData);*/
-
     xmlHttpRequest.open("POST", "http://localhost:8080/typeVeriCode/3", true); // 创建http请求，并指定请求得方法（get）、url（https://www.runoob.com/try/ajax/ajax_info.txt）以及验证信息
-    xmlHttpRequest.send(veriCode); // 发送请求
-
-    //Post email to server
+    xmlHttpRequest.setRequestHeader("Content-Type", "application/json");
+    xmlHttpRequest.send(veriCodeJson); // 发送请求
 }
 
 function getCaptcha(event) {
     event.preventDefault();
-    let xhr=new XMLHttpRequest();
+    let xhr = new XMLHttpRequest();
     let email = document.getElementById("email").value;
-    xhr.open("GET", "http://localhost:8080/sendVeriCode/3?email="+ email);
+    xhr.open("GET", "http://localhost:8080/sendVeriCode/3?email=" + email);
     xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.withCredentials=true;
-    
+    xhr.withCredentials = true;
     xhr.onload = function () {
         let response = JSON.parse(xhr.responseText);
-        if (xhr.readyState===4&&xhr.status === 200) {
+        if (xhr.readyState === 4 && xhr.status === 200) {
             alert("已发送验证码");
-        }else{
-            console.log("错误"+response.textContent);
+        } else {
+            console.log("错误" + response.textContent);
         }
     };
     xhr.send();
@@ -53,7 +46,7 @@ function getCaptcha(event) {
 
 
 function redirectToIndexPage(username) {
-    window.location.href = "../../index.html?username="+encodeURIComponent(username);
+    window.location.href = "../index.html?username=" + encodeURIComponent(username);
 }
 
 function redirectToLoginPage() {
