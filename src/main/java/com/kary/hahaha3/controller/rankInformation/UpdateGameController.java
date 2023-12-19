4e274ea12f5f59be41418ee08a6a994e602904bf
karywood <834479572@qq.com>
2023-12-19 22:49:23 +0800

    修改添加游戏controller

--- src/main/java/com/kary/hahaha3/controller/rankInformation/UpdateGameController.java
+++ src/main/java/com/kary/hahaha3/controller/rankInformation/UpdateGameController.java
@@ -1,6 +1,7 @@
 package com.kary.hahaha3.controller.rankInformation;
 
 import com.kary.hahaha3.pojo.JsonResult;
+import com.kary.hahaha3.pojo.vo.AddNewGameJSON;
 import com.kary.hahaha3.pojo.vo.NewGameRecordJSON;
 import com.kary.hahaha3.service.GameRecordService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -8,6 +9,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +25,7 @@ public class UpdateGameController {
     GameRecordService gameRecordService;
     @PostMapping("/recordNewMatch")
     @Operation(summary = "记录新的一场比赛",description = "返回1表示更新成功，返回0表示更新失败。把游戏里updateDatabase的参数全部用json发进来")
-    public JsonResult recordNewMatch(NewGameRecordJSON json){
+    public JsonResult recordNewMatch(@RequestBody NewGameRecordJSON json){
         Integer res= gameRecordService.recordNewMatch(json.getMaxGameId(),json.getDuration(),json.getUsername(),json.getKill(),json.getDeath(),json.getScoreGain(),json.getAssist(),json.getTakeDamage(),json.getTakenDamage(),json.getMvpPlayer(),json.getGameMode());
         if(res!=1){
             return JsonResult.error("更新失败");
@@ -32,8 +34,8 @@ public class UpdateGameController {
     }
     @PostMapping("/addNewGame")
     @Operation(summary = "添加新的一场游戏",description = "无需使用，只是测试而已。插件里有这个功能")
-    public JsonResult addNewGame(Integer type,Integer gameId,Long duration,String mvpPlayer){
-        Integer res= gameRecordService.addNewGame(type,gameId,duration,mvpPlayer);
+    public JsonResult addNewGame(@RequestBody AddNewGameJSON json){
+        Integer res= gameRecordService.addNewGame(json.getType(),json.getGameId(),json.getDuration(),json.getMvpPlayer());
         if(res!=1){
             return JsonResult.error("添加失败");
         }
