7822613e86d53093b85d69e8637cf5e318c4b688
Danqingyifan <danqingyifan@gmail.com>
2023-12-25 17:58:17 +0800

    update

--- src/main/resources/static/scripts/square.js
+++ src/main/resources/static/scripts/square.js
@@ -1,3 +1,7 @@
+window.onload = function () {
+    createMatchCard();
+}
+
 function redirectToIndex(event) {
     event.preventDefault();
     let myUsername = sessionStorage.getItem('myUsername');
@@ -15,4 +19,68 @@ function redirectToMyPersonal(event) {
 function redirectToSquare(event) {
     event.preventDefault();
     window.location.href = "../pages/square.html?username=" + encodeURIComponent(username);
+}
+
+function createMatchCard() {
+    
+
+
+    const liveUpdate = document.getElementById("live_game_updates");
+    liveUpdate.innerHTML = "";
+    liveUpdate.innerHTML = `
+    <div class="match-card">
+        <div class="player" id="player1">
+            <img src="../images/portrait_3.jpg" alt="Player 1">
+            <span>玩家1名字</span>
+        </div>
+        <div class="score">3 : 1</div>
+        <div class="player" id="player2">
+            <img src="../images/portrait_5.jpg" alt="Player 2">
+            <span>玩家2名字</span>
+        </div>
+    </div>`;
+}
+function allRanks() {
+    soloRank();
+    brawlRank();
+    scoreRank();
+}
+
+function soloRank() {
+    fetch(`http://localhost:8080/ranks/get1v1RankingOrder`)
+        .then(response => {
+            console.log(response);
+            console.log(response.textContent);
+            return response.json();
+        }).then(data => {
+            console.log(data.data);
+
+        })
+        .catch(error => console.log(error));
+}
+function brawlRank() {
+    fetch(`http://localhost:8080/ranks/getBrawlRankingOrder`)
+        .then(response => {
+            console.log(response);
+            console.log(response.textContent);
+            return response.json();
+        }).then(data => {
+            console.log(data.data);
+            for (let i = 0; i < 7; i++) {
+
+            }
+        })
+        .catch(error => console.log(error));
+}
+function scoreRank() {
+    fetch(`http://localhost:8080/ranks/getTotalScoreRankingOrder`)
+        .then(response => {
+            console.log(response);
+            console.log(response.textContent);
+            return response.json();
+        }).then(data => {
+            console.log(data.data);
+
+        })
+        .catch(error => console.log(error));
 }
